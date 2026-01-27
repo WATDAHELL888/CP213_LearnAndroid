80ae930d8d9953767b6b44af5f2d35188be2fe80
WATDAHELL888 <tangza1123a@gmail.com>
2026-01-27 16:23:27 +0700

    HP Bar

--- Lab/app/src/main/java/com/example/a522lablearnandroid/MainActivity.kt
+++ Lab/app/src/main/java/com/example/a522lablearnandroid/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.a522lablearnandroid.ui.theme._522LabLearnAndroidTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +26,21 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            _522LabLearnAndroidTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+            Column(modifier = Modifier.fillMaxSize().background(color = Color.Gray).padding(32.dp)) {
+                // hp bar
+                Box(modifier = Modifier.fillMaxWidth().height(32.dp).background(color = Color.White)) {
+                    Text(
+                        text = "HP",
+                        modifier = Modifier.align(alignment = Alignment.CenterStart).fillMaxWidth(fraction = 0.22f).background(color = Color.Red).padding(8.dp)
                     )
+                }
+
+                // image
+
+                // status
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    _522LabLearnAndroidTheme {
-        Greeting("Android")
-    }
-}
--- Lab/app/src/main/java/com/example/a522lablearnandroid/MainActivity2.kt
+++ Lab/app/src/main/java/com/example/a522lablearnandroid/MainActivity2.kt
@@ -4,13 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.a522lablearnandroid.ui.theme._522LabLearnAndroidTheme
 
 class MainActivity2 : ComponentActivity() {
@@ -32,11 +44,21 @@ class MainActivity2 : ComponentActivity() {
 
 @Composable
 fun Greeting2(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    var inputText by remember { mutableStateOf(value = "") } // c
+    Column {
+            Text(
+                text = "Hello $name! say = "+inputText,
+                modifier = modifier
+            )
+        TextField(
+            value = inputText,
+            onValueChange = { // c
+                inputText = it
+            }
+        )
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
